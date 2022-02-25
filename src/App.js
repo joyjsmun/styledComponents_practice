2a8fdf0c70c313d4230eb7e298d8e445052457d2
joyjsmun <joy.js.mun@gmail.com>
2022-02-24 17:09:56 -0800

    ADD: using keyframes for animation

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 const Wrapped = styled.div`
 display: flex;
 `
 
-const Input = styled.input.attrs({required:true})`
-background-color: tomato;
-color:white;
+const BoxAnimation = keyframes`
+0%{
+transform: scale(0);
+}50% {
+transform: scale(2);
 border-radius: 50px;
-border: 0;
+}
+100%{
+  transform: scale(1);
+}
+
+`
+
+const Box = styled.div`
+height: 200px;
+width:200px;
+background-color: tomato;
+animation: ${BoxAnimation} 1s linear infinite;
 `
 
 function App() {
   return (
     <Wrapped>
-     <Input />
-     <Input />
-     <Input />
+      <Box/>
     </Wrapped>
   );
 }
