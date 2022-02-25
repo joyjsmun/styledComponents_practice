3eae7f20367e28dece4c738a3444fb226dd1b6a1
joyjsmun <joy.js.mun@gmail.com>
2022-02-24 16:54:32 -0800

    ADD: adapting and extending props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
+import styled from "styled-components";
+
+const Wrapped = styled.div`
+display: flex;
+
+`
+
+const Box = styled.div`
+background-color: ${(props) => props.bgColor};
+height:100px;
+width: 100px;
+`
+
+const Circle = styled(Box)`
+border-radius: 50px;
+`
+
 function App() {
   return (
-    <div>Hello</div>
+    <Wrapped>
+      <Box bgColor="red"></Box>
+      <Circle bgColor="blue"></Circle>
+    </Wrapped>
   );
 }
 
