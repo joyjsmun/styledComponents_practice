2a0806eafdaad39b7ce1d69f3eeced1f5421afaa
joyjsmun <joy.js.mun@gmail.com>
2022-02-25 19:06:44 -0800

    ADD: using Form with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
+import React, { useState } from "react";
 import styled, { keyframes } from "styled-components";
-import Circle from "./Circle";
+
 
 
 function App() {
-  return (
-<>
-  <Circle bgColor="red" text="You are super awesome"/>
-  <Circle bgColor="yellow" borderColor="red"/>
-</>
+  const [value,setValue] = useState("");
+  const onChange = (event:React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget:{value},
+    } = event;
+    setValue(value);
+  };
 
+  const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("Hello ",value)
+  }
+  return (
+<div>
+  <form onSubmit={onSubmit}>
+    <input onChange={onChange} value={value} type="text" placeholder="username"></input>
+    <button>Log in </button>
+  </form>
+</div>
   );
 }
 
